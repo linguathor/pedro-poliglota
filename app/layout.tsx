a0c85bbe3688fb2e1a1cbb6a34941f1d6c7b33a4
David Allen Martin II <davidamartin2@gmail.com>
2026-02-17 13:26:06 +0100

    chore: add @vercel/analytics and include Analytics in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics gaId="G-4JEJ87FXN6" />
+        <Analytics />
       </body>
-      <GoogleAnalytics gaId="G-4JEJ87FXN6" />
     </html>
   );
 }
